d87e556a0d5ed6acd4198f33241e45ca86a3ffbe
Sebastian Scherbes <github@scherbes.de>
2022-09-24 11:35:35 +0200

    create card component

---
+++ src/components/card/Card.js
@@ -0,0 +1,11 @@
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <li className="card-box">
+      <button className="card-button">#ccc</button>
+    </li>
+  );
+};
+
+export default Card;
