3164227376e7d61252b21cfaa4fb25beace932b8
Sebastian Scherbes <github@scherbes.de>
2022-09-25 14:03:05 +0200

    add edit-function to card-component

--- src/components/cardlist/CardList.js
+++ src/components/cardlist/CardList.js
@@ -1,11 +1,16 @@
 import "./CardList.css";
 import Card from "../card/Card";
 
-const CardList = ({ cards }) => {
+const CardList = ({ cards, onChangeHandler }) => {
   return (
     <ul className="card-list">
       {cards.map((card) => (
-        <Card key={card.id} hexCode={card.hexCode} />
+        <Card
+          key={card.id}
+          id={card.id}
+          hexCode={card.hexCode}
+          onChangeHandler={onChangeHandler}
+        />
       ))}
     </ul>
   );
