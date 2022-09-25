498e6f06f9a9d81adc10fe146e4170a3e77a2c96
Sebastian Scherbes <github@scherbes.de>
2022-09-25 14:27:24 +0200

    add delete functionality

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,8 +1,12 @@
 import "./Card.css";
+import deleteIcon from "../../img/delete.svg";
 
-const Card = ({ id, hexCode, onChangeHandler }) => {
+const Card = ({ id, hexCode, onChangeHandler, onDeleteHandler }) => {
   return (
     <li className="card__box" style={{ backgroundColor: hexCode }}>
+      <button className="card__delete" onClick={() => onDeleteHandler(id)}>
+        <img className="card__delete__icon" src={deleteIcon} alt="" />
+      </button>
       <input
         className="card__input"
         onChange={(event) => onChangeHandler(id, event.target.value)}
